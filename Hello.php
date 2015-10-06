1abc809388fab1770f5a93a3d10aa04851c43b01
Sergi <spineda1994@gmail.com>
2015-10-06 19:43:50 +0200

    composer

---
+++ Hello.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sergi
+ * Date: 6/10/15
+ * Time: 19:32
+ */
+class Hello
+{
+
+    public function say_hello()
+    {
+        echo "Hola món";
+    }
+}
---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . "/Hello.php";
+/**
+ * Created by PhpStorm.
+ * User: sergi
+ * Date: 6/10/15
+ * Time: 19:33
+ */
+
+$greetings = new Hello();
+
+$greetings->say_Hello();
